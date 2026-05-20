4597f079d3d83c5e1e32d2082c70dee29237fb76
Borrax <borrax@protonmail.com>
2026-05-20 22:05:29 +0300

    Removing reference of nonce in the file encrypter

--- src/aes_gcm.rs
+++ src/aes_gcm.rs
@@ -380,7 +380,7 @@ pub fn generate_nonce() -> [u8; 12] {
 pub fn encrypt_file(input_path: &str, output_path: &str, key: &[u8;32], nonce: &[u8; 12], aad: &[u8]) -> std::io::Result<()> {
     let raw_file = read(input_path)?;
 
-    let (encrypted, tag) = aes_gcm_encrypt(key, &nonce, &raw_file, aad);
+    let (encrypted, tag) = aes_gcm_encrypt(key, nonce, &raw_file, aad);
 
     let mut output_bytes = Vec::new();
     output_bytes.extend_from_slice(nonce);
