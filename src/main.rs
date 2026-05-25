8bc331444675a69527695ffde7cec61bf366d7fc
Borrax <borrax@protonmail.com>
2026-05-25 16:55:42 +0300

    Using the new run_application function in main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use file_encrypter::aes_gcm::{encrypt_file, generate_nonce, read_terminal};
+use file_encrypter::aes_gcm::{run_application, read_terminal};
 
 fn main() {
-    let (input_path, output_path) = read_terminal();
-    let key = b"test keytest ketest ketest keyyy";
-    let nonce = generate_nonce();
-    let aad = b"my_checksum";
+    let input_data = read_terminal();
 
-    let _ = encrypt_file(&input_path, &output_path, key, &nonce, aad);
-
-    // let text_byte = b"Hello there";
+    run_application(&input_data);
 }
