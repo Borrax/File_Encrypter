049bca80958180fc020ed58b81614cafe90202aa
Borrax <borrax@protonmail.com>
2026-05-26 19:57:15 +0300

    Integration test to check decrypted file existing

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4,20 +4,33 @@ use file_encrypter::aes_gcm::{run_application, UserInputData};
 #[test]
 fn test_application_simple_file() {
     let input_path = "./tests/test_file.txt";
-    let output_path = "./tests/encrypted_test_file";
+    let output_path_enc = "./tests/encrypted_test_file";
+    let output_path_dec = "./tests/decrypted_test_file";
 
-    if fs::exists(output_path).unwrap() {
-        fs::remove_file(output_path).unwrap();
+    if fs::exists(output_path_enc).unwrap() {
+        fs::remove_file(output_path_enc).unwrap();
+    }
+
+    if fs::exists(output_path_dec).unwrap() {
+        fs::remove_file(output_path_dec).unwrap();
     }
 
     let key = b"12345678901234567890123456789012";
 
     let mut input_data = UserInputData::default();
     input_data.input_path = Some(input_path.to_string());
-    input_data.output_path = output_path.to_string();
+    input_data.output_path = output_path_enc.to_string();
     input_data.key = Some(key.clone());
 
     run_application(&input_data);
 
-    assert!(fs::exists(output_path).unwrap(), "Encrypted file does not exist")
+    assert!(fs::exists(output_path_enc).unwrap(), "Encrypted file does not exist");
+
+    input_data.input_path = Some(output_path_enc.to_string());
+    input_data.output_path = output_path_dec.to_string();
+    input_data.should_encrypt = false;
+
+    run_application(&input_data);
+
+    assert!(fs::exists(output_path_dec).unwrap(), "Decrypted file does not exist");
 }
