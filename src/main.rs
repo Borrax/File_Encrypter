406d89c908ead4a85b4413bc7d9e6610b2108dc3
Borrax <borrax@protonmail.com>
2026-06-06 14:10:32 +0300

    Currect matching of the terminal output in the main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use file_encrypter::aes_gcm::{run_application, read_terminal};
 
 fn main() {
-    let input_data = read_terminal(std::io::stdin().lock());
-    match input_data {
+    let input_data = match read_terminal(std::io::stdin().lock()) {
         Ok(val) => val,
         Err(e) => {
             eprintln!("{}", e);
